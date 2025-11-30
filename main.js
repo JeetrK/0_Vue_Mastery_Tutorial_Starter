d5f3df48b12f982b95647a096cb09ca68a5ee5a0
JeetrK <526HKlein@frhsd.com>
2025-11-30 21:04:35 +0000

    list rendering

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
-      inventory: 8
+      inStock: true,
+      inventory: 8,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green',},
+        { id: 2235, color: 'blue',  },
+      ]
     }
   }
 })
